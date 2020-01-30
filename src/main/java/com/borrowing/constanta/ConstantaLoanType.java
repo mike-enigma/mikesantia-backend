04405c5a4b0bfc1edbec8575afc622c8bd9e5029
Mike <mikesantia.6297@gmail.com>
2020-01-30 16:02:24 +0700

    [FEATURE] Constanta LoanType

---
+++ src/main/java/com/borrowing/constanta/ConstantaLoanType.java
@@ -0,0 +1,8 @@
+package com.borrowing.constanta;
+
+public class ConstantaLoanType {
+    public static final String ENTITY_LOAN_TYPE="mst_loan_type";
+    public static final String JOIN_LOAN_TYPE="loan_type";
+    public static final String MESSAGE_LOAN_TYPE_NOT_FOUND="LoanTypeId %s Not found";
+
+}
