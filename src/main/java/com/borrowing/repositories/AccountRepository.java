7285d1fccc3dcae8a39c13c1fd72d1eb85e8a6fb
Mike <mikesantia.6297@gmail.com>
2020-01-30 15:43:00 +0700

    [FEATURE] Account Repository

---
+++ src/main/java/com/borrowing/repositories/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.borrowing.repositories;
+
+import com.borrowing.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account,String> {
+}
