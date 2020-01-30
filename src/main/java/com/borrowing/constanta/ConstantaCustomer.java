37f6d69e9f8572ab555e1a57b0ed236d96366857
Mike <mikesantia.6297@gmail.com>
2020-01-30 16:02:53 +0700

    [FEATURE] Constanta Customer

---
+++ src/main/java/com/borrowing/constanta/ConstantaCustomer.java
@@ -0,0 +1,7 @@
+package com.borrowing.constanta;
+
+public class ConstantaCustomer {
+    public static final String ENTITY_CUSTOMER="mst_customer";
+    public static final String MESSAGE_CIF_NOT_FOUND="Cif Customer %s Not found";
+    public static final String JOIN_CUSTOMER="cif";
+}
