8b3d3785a80749002093cc4500f76792b1846ad8
Mike <mikesantia.6297@gmail.com>
2020-01-30 16:03:25 +0700

    [FEATURE] Constanta AccountType

---
+++ src/main/java/com/borrowing/constanta/ConstantaAccountType.java
@@ -0,0 +1,8 @@
+package com.borrowing.constanta;
+
+public class ConstantaAccountType {
+    public static final String ENTITY_ACCOUNT_TYPE="mst_account_type";
+    public static final String JOIN_ENTITY="account_type";
+    public static final String MESSAGE_ACCOUNT_TYPE_NOT_FOUND="Id AccountType %s Not found";
+
+}
