69fcdd48df8f6b45c3ec0c9f4ee5efaf4023bd78
Mike <mikesantia.6297@gmail.com>
2020-01-30 16:03:38 +0700

    [FEATURE] Constanta Account

---
+++ src/main/java/com/borrowing/constanta/ConstantaAccount.java
@@ -0,0 +1,7 @@
+package com.borrowing.constanta;
+
+public class ConstantaAccount {
+    public static final String ENTITY_ACCOUNT="mst_account";
+    public static final String MESSAGE_ACCOUNT_TYPE_NOT_FOUND="Account Number %s Not found";
+
+}
