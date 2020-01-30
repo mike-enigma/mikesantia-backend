5f8757fdee6173383106c3dd1f59ee4a9f0cede0
Mike <mikesantia.6297@gmail.com>
2020-01-30 16:03:11 +0700

    [FEATURE] Constanta CollectStatus

---
+++ src/main/java/com/borrowing/constanta/ConstantaCollectStatus.java
@@ -0,0 +1,7 @@
+package com.borrowing.constanta;
+
+public class ConstantaCollectStatus {
+    public static final String JOIN_COLLECT_STATUS = "collect_status";
+    public static final String ENTITY_COLLECT_STATUS="mst_collect_status";
+    public static final String MESSAGE_COLLECT_STATUS_NOT_FOUND="Id CollectStatus %s Not found";
+}
