393db02360754a988c0db82c67cbe62491932813
Mike <mikesantia.6297@gmail.com>
2020-01-30 15:42:11 +0700

    [FEATURE] Customer Repository

--- src/main/java/com/borrowing/repositories/CustomerRepository.java
+++ src/main/java/com/borrowing/repositories/CustomerRepository.java
@@ -4,6 +4,9 @@ import com.borrowing.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer,String> {
+    List<Customer> findByFirstName(String firstName);
 }
