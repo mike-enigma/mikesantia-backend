78bf5b613cb3265cececca619076280ad6bb4574
Mike <mikesantia.6297@gmail.com>
2020-01-30 16:02:36 +0700

    [FEATURE] Constanta Loan

---
+++ src/main/java/com/borrowing/constanta/ConstantaLoan.java
@@ -0,0 +1,6 @@
+package com.borrowing.constanta;
+
+public class ConstantaLoan {
+    public static final String ENTITY_LOAN = "trx_loan";
+    public static final String MESSAGE_LOAN_ID_NOT_FOUND="Loan Id %s Not found";
+}
